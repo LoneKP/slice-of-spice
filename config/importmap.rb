cff4a852d5cbc78ab2b50c182e8960f3bd465185
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2025-05-08 15:21:42 +0200

    don't pin all

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,12 @@ pin "@hotwired/stimulus",           to: "stimulus.min.js",       preload: true
 pin "@hotwired/stimulus-loading",   to: "stimulus-loading.js",   preload: true
 
 # Controllers
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true
+pin "controllers/application", preload: true
+pin "controllers/hello_controller", preload: true
+pin "controllers/sortable_controller", preload: true
+pin "controllers/flash_controller", preload: true
+pin "controllers/dropdown_controller", preload: true
+pin "controllers/index", preload: true
 
 # Pin npm packages by running ./bin/importmap
 pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.2/Sortable.min.js", preload: true
