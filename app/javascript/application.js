9168f9b2bec08791b46abfd20a2e3d61c7276fad
LoneKP <lone.kjeldgaard.pedersen@gmail.com>
2025-05-08 14:56:29 +0200

    does it work now?

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // app/javascript/application.js
 import "@hotwired/turbo-rails";
+import "@hotwired/stimulus";
 import "@hotwired/stimulus-loading";
+import "controllers/application";
 import "controllers";
 import "sortablejs";
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,8 @@ pin "@hotwired/turbo-rails",        to: "turbo.min.js",          preload: true
 pin "@hotwired/stimulus",           to: "stimulus.min.js",       preload: true
 pin "@hotwired/stimulus-loading",   to: "stimulus-loading.js",   preload: true
 
-# All controllers in app/javascript/controllers/**/*
+# Controllers
+pin "controllers/application", to: "controllers/application.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 
 # Pin npm packages by running ./bin/importmap
